24ebdb92c0af19c975b0e6bfed73cbe3dc0fc0de
Abyou <emandaabyou@gmail.com>
2022-07-25 12:25:28 -0500

    While deploying the app on Github, the base name had been added to the router in app.js. This has now been removed so that the app can be run locally.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router basename="/react-portfolio">
+      <Router>
         <Navbar />
         <Routes>
           <Route exast path="/" />
